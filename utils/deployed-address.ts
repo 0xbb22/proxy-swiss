69a4c380d4327c4e8f3a14615210d1389f808308
0xbb22 <slametwibowo1922@gmail.com>
2024-08-12 05:32:53 +0000

    PROXY

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x94D022B816E4f924CE8164631d1dEFEEAfa9b74a'
+export const ProxyAdmin = '0x53a2C575289895aBe027E6f5bce53695Cf0d71e0'
+export const SWTRImplementation = '0xb78C6fa8236f366B88b9f818B4a45D46a8A261E6'
+    
